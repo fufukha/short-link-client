8b784f3b7a284b493e29014d6d7ec50e10ee1bae
fufukha <fufukha@users.noreply.github.com>
2020-11-22 13:42:08 -0500

    added basic styles to copy button

--- src/components/CopyButton/index.js
+++ src/components/CopyButton/index.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import Button from "@material-ui/core/Button";
 
 const CopyButton = ({ text }) => {
   const [isCopied, setIsCopied] = useState(false);
 
   return (
     <CopyToClipboard text={text} onCopy={() => setIsCopied(true)}>
-      <button>Copy</button>
+      <Button>Copy</Button>
     </CopyToClipboard>
   );
 };
