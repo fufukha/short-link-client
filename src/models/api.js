5e6d7729f7d77b5ba811139e21dc5eec4016ac73
fufukha <wafakhalid29@gmail.com>
2020-11-28 16:54:01 -0500

    fix gql in order to use Hooks

--- src/models/api.js
+++ src/models/api.js
@@ -1,42 +1,29 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { gql } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql
+} from '@apollo/client';
 
 export const client = new ApolloClient({
   uri: "https://bqreu.sse.codesandbox.io/",
   cache: new InMemoryCache()
 });
 
-export const getLinks = () => {
-  return client.query({
-    query: gql`
-      query {
-        getLinks {
-          url
-          name
-        }
-      }
-    `
-  });
-};
-
-export const addLink = (name, url) => {
-  return client.mutate({
-    mutation: gql`
-      mutation {
-        createLink(name: name, url: url) {
-          name
-          url
-        }
-      }
-    `
-  });
-};
-
-export const CREATE_LINK = gql`
-  mutation createLink($name: String, $url: String!) {
-    createLink(name: $name, url: $url) {
-      id
-      type
+export const LINKS = gql`
+  query {
+    getLinks {
+      name
+      url
     }
   }
-`;
+`
+
+export const CREATE_LINK = gql`
+  mutation CreateLink($name: String!, $url: String!) {
+    createLink(name: $name, url: $url) {
+      id
+      name
+      url
+    }
+  }
+`
