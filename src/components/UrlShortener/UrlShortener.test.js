f22e752899fb619e6d43395db14348f2f0cb53c7
fufukha <wafakhalid29@gmail.com>
2020-11-30 18:02:35 -0500

    test UrlShortener renders loading state initially

--- src/components/UrlShortener/UrlShortener.test.js
+++ src/components/UrlShortener/UrlShortener.test.js
@@ -1,23 +1,48 @@
 import React from "react";
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import UrlShortener from './index'
 import { ThemeProvider } from '@material-ui/core/styles'
 import theme from '../../theme'
 import { MockedProvider } from '@apollo/client/testing';
+import { CREATE_LINK } from "../../models/api";
 
-const component = (
+const url = "url.com"
+const addedLink = { url, id: 1, name: '', slug: 'xxxx'}
+const mocks = [
+  {
+    request: {
+      query: CREATE_LINK,
+      variables: { url }
+    },
+    result: { data: { addedLink } },
+  },
+]
+
+const component = (mocks) => (
   <ThemeProvider theme={theme}>
-    <MockedProvider mocks={[]}>
+    <MockedProvider mocks={mocks} addTypename={false}>
       <UrlShortener />
     </MockedProvider>
   </ThemeProvider>
 )
 
 it('UrlShortener component renders without crashing', () => {
-  const { getByText, getByLabelText } = render(component)
+  const { getByText, getByLabelText } = render(component([]))
 
   getByLabelText('Place URL here')
   getByLabelText('Place custom alias here')
   getByLabelText('Name your URL')
   getByText('Shorten')
 });
+
+it('UrlShortener component renders loading state initially', () => {
+  const { getByText, getByLabelText, getByRole } = render(component(mocks))
+
+  const input = getByLabelText('Place URL here')
+  const button = getByText('Shorten')
+
+  fireEvent.change(input, { target: { value: url }})
+  fireEvent.click(button)
+
+  getByRole('progressbar')
+})
