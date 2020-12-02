808da8267cc031d30b1768dfc999ef8e7c3c6d42
fufukha <wafakhalid29@gmail.com>
2020-12-01 22:04:31 -0500

    Removed unused variable & wrapped input in form element

--- src/components/UrlShortener/index.js
+++ src/components/UrlShortener/index.js
@@ -36,7 +36,6 @@ const UrlShortener = () => {
   const [url, setUrl] = useState('');
   const [shortUrlData, setShortUrlData] = useState();
   const [alias, setAlias] = useState("")
-  const [name, setName] = useState('')
   const [urlName, setUrlName] = useState('')
   const [isError, setIsError] = useState(false)
 
@@ -67,7 +66,6 @@ const UrlShortener = () => {
   const handleChange = (e, type) => {
     if(type === 'url') setUrl(e.target.value);
     if(type === 'alias') setAlias(e.target.value);
-    if(type === 'name') setName(e.target.value)
     if(type === 'urlName') setUrlName(e.target.value)
   }
 
@@ -87,64 +85,69 @@ const UrlShortener = () => {
   const nameLabel= 'Name your URL'
 
   return (
-    <Grid container justify="space-around" spacing={3}>
-      <Grid item xs={12}>
-        <TextField
-          className={classes.textField}
-          variant="outlined"
-          error={isError}
-          fullWidth
-          type="text"
-          required
-          label="Required"
-          placeholder="Place URL here"
-          inputProps={{ label: urlLabel, 'aria-label': urlLabel}}
-          InputLabelProps={{shrink: true}}
-          value={url}
-          onChange={e => handleChange(e, 'url')}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <TextField
-          className={classes.textField}
-          variant="outlined"
-          fullWidth
-          type="text"
-          required={false}
-          label="Custom URL alias (optional)"
-          placeholder="Place alias here"
-          inputProps={{ label: slugLabel, 'aria-label': slugLabel}}
-          InputLabelProps={{shrink: true}}
-          value={alias}
-          onChange={e => handleChange(e, 'alias')}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <TextField
-          className={classes.textField}
-          variant="outlined"
-          fullWidth
-          type="text"
-          required={false}
-          label="Name (optional)"
-          placeholder="Name your URL"
-          inputProps={{ label: nameLabel, 'aria-label': nameLabel}}
-          InputLabelProps={{shrink: true}}
-          value={urlName}
-          onChange={e => handleChange(e, 'urlName')}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <Button
-          className={classes.button}
-          variant="contained"
-          fullWidth
-          size="large"
-          color="primary"
-          onClick={handleClick}>
-          Shorten
-        </Button>
-      </Grid>
+    <>
+      <form autoComplete="off">
+        <Grid container justify="space-around" spacing={3}>
+          <Grid item xs={12}>
+            <TextField
+              className={classes.textField}
+              variant="outlined"
+              error={isError}
+              data-testid={isError}
+              fullWidth
+              type="text"
+              required
+              label="Required"
+              placeholder="Place URL here"
+              inputProps={{ label: urlLabel, 'aria-label': urlLabel}}
+              InputLabelProps={{shrink: true}}
+              value={url}
+              onChange={e => handleChange(e, 'url')}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              className={classes.textField}
+              variant="outlined"
+              fullWidth
+              type="text"
+              required={false}
+              label="Custom URL alias (optional)"
+              placeholder="Place alias here"
+              inputProps={{ label: slugLabel, 'aria-label': slugLabel}}
+              InputLabelProps={{shrink: true}}
+              value={alias}
+              onChange={e => handleChange(e, 'alias')}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              className={classes.textField}
+              variant="outlined"
+              fullWidth
+              type="text"
+              required={false}
+              label="Name (optional)"
+              placeholder="Name your URL"
+              inputProps={{ label: nameLabel, 'aria-label': nameLabel}}
+              InputLabelProps={{shrink: true}}
+              value={urlName}
+              onChange={e => handleChange(e, 'urlName')}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              className={classes.button}
+              variant="contained"
+              fullWidth
+              size="large"
+              color="primary"
+              onClick={handleClick}>
+              Shorten
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
       {error && <p>error.message</p>}
       {loading && (
         <Box className={classes.loader}>
@@ -157,7 +160,7 @@ const UrlShortener = () => {
           <CopyButton url={shortUrlData.url} name={shortUrlData.name} />
         </Box>
       )}
-    </Grid>
+    </>
   );
 };
 
